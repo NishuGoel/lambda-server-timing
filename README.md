c71ef22fb0b9d7992880488013f838cf7ea22cf7
Nishu Goel <n.goel@epilot.cloud>
2023-12-27 02:03:07 +0100

    ⭐

---
+++ README.md
@@ -0,0 +1,5 @@
+## Lambda Server-Timing Middleware
+
+Enables Lambdas to return responses with Server-Timing Header allowing to to pass request-specific timings from the backend to the browser.
+
+Enables a server to communicate performance metrics about the request-response cycle to the user agent. It also standardizes a JavaScript interface to enable applications to collect, process, and act on these metrics to optimize application delivery.
---
+++ index.ts
@@ -0,0 +1,125 @@
+import Log from '@dazn/lambda-powertools-logger';
+import middy from '@middy/core';
+import * as Lambda from 'aws-lambda';
+
+/**
+ * @returns a lambda middleware that adds a Server-Timing header to the response
+ */
+export const withServerTimings = () => {
+    return {
+      // add Server-Timing header to response
+      after: (handler: middy.Request) => {
+        const response = handler.response as Lambda.APIGatewayProxyStructuredResultV2;
+        // get timings from request
+
+        try {
+          const headers: unknown[] = [];
+          const timings = getServerTimingHeader(headers);
+
+          response.headers = {
+            ...response.headers,
+            'Server-Timing': timings,
+          };
+        } catch (e) {
+          Log.debug(`Error: Could not record server timings - ${e}`);
+        }
+      },
+    };
+};
+
+/**
+ * @param name 
+ * @param description 
+ * Records the start time of a metric
+ */
+export const startTime = (name: string, description?: string) => {
+  try {
+    if (typeof name !== 'string') {
+      return console.warn('Metric name is not string');
+    }
+
+    timer(name, description);
+  } catch (e) {
+    Log.debug(`Error: Could not record start time ${name} - ${e}`);
+  }
+};
+
+/**
+ * @param name 
+ * @param description 
+ * Records the duration of a metric and sets a metric timing
+ */
+export const endTime = (name: string, description?: string) => {
+  try {
+    if (typeof name !== 'string') {
+      return console.warn('Metric name is not string');
+    }
+
+    const obj = timerEnd(name);
+    if (!obj) {
+      return;
+    }
+    setMetric({
+      name: obj.name as string,
+      description: obj.description as string,
+      value: obj.value as [number, number],
+    });
+  } catch (e) {
+    Log.debug(`Error: Could not record end time for ${name} - ${e}`);
+  }
+};
+
+// measure time
+interface TimeObject {
+  name: string;
+  description: string;
+  value: [number, number];
+}
+const times = new Map<string, Record<string, unknown>>();
+const timer = (name: string, description?: string) => {
+  times.set(name, {
+    name,
+    description: description || '',
+    startTime: process.hrtime(),
+  });
+};
+
+const timerEnd = (name: string, description?: string) => {
+  const timeObj = times.get(name);
+  if (!timeObj) {
+    return console.warn(`No such name ${name}`);
+  }
+  const duration = process.hrtime(timeObj.startTime as [number, number]);
+  const value = duration[0] * 1e3 + duration[1] * 1e-6;
+  timeObj.value = value;
+  times.delete(name);
+
+  return timeObj;
+};
+
+let tempHeaders: unknown[] = [];
+
+const setMetric = ({ name, value, description }: TimeObject) => {
+  if (typeof name !== 'string') {
+    return console.warn('1st argument name is not string');
+  }
+  if (typeof value !== 'number') {
+    return console.warn('2nd argument value is not number');
+  }
+
+  const dur = value;
+
+  const metric =
+    typeof description !== 'string' || !description
+      ? `${name}; dur=${dur ?? 0}`
+      : `${name}; dur=${dur}; desc="${description}"`;
+
+  tempHeaders.push(metric);
+};
+
+const getServerTimingHeader = (headers: unknown[]) => {
+  headers.push(tempHeaders?.toString());
+  tempHeaders = [];
+
+  return headers?.toString();
+};
